dfeb5a532fffd46ae3eabb9ad6c930369b810831
Muhrofin <masovin.projects@gmail.com>
2021-09-15 21:52:54 +0700

    [D] - Main page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:foodmarket/shared/shared.dart';
+import 'package:foodmarket/ui/widgets/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:get/get.dart';
@@ -11,3 +14,4 @@ part 'general_page.dart';
 part 'sign_in_page.dart';
 part 'sign_up_page.dart';
 part 'address_page.dart';
+part 'main_page.dart';
